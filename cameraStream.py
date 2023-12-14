4c713012d8ee8834ba70a96128c4226dff4356eb
Josh Graff <github@joshuagraff.com>
2023-12-14 12:25:09 -0500

    reset

--- cameraStream.py
+++ cameraStream.py
@@ -3,7 +3,7 @@
 # http://picamera.readthedocs.io/en/latest/recipes2.html#web-streaming
 
 import io
-import picamera2
+import picamera
 import logging
 import socketserver
 from threading import Condition
@@ -83,19 +83,13 @@ class StreamingServer(socketserver.ThreadingMixIn, server.HTTPServer):
     daemon_threads = True
 
 
-with picamera2.Picamera2() as camera:
-    capture_config = camera.create_still_configuration(
-        main={"size": (640, 480)},
-        lores={"size": (640, 480)},
-        display="lores",
-    )
-    camera.configure(capture_config)
+with picamera.PiCamera(resolution="640x480", framerate=24) as camera:
     output = StreamingOutput()
     # Uncomment the next line to change your Pi's Camera rotation (in degrees)
     # camera.rotation = 90
     camera.start_recording(output, format="mjpeg")
     try:
-        address = ("", 80)
+        address = ("", 8000)
         server = StreamingServer(address, StreamingHandler)
         server.serve_forever()
     finally:
