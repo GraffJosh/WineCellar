3bc6646fd5ac834fa22b9e1b5718ce563a81fd5e
Josh Graff <github@joshuagraff.com>
2023-12-14 12:48:58 -0500

    testing new resolutions

--- cameraPrinter.py
+++ cameraPrinter.py
@@ -16,7 +16,7 @@ import mqttPrinter
 
 
 class CameraPrinter:
-    def __init__(self, daemon=True, pixelWidth=1000, pixelHeight=512, video=False) -> None:
+    def __init__(self, daemon=True, pixelWidth=1000, pixelHeight=1024, video=False) -> None:
         self.red_led = red_led
         self.blue_led = LED(PIN_BLUE_LED)
         self.green_led = LED(PIN_GREEN_LED)
