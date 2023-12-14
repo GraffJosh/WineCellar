653409b9fad5649631b0b17884c120c93a887002
Josh Graff <github@joshuagraff.com>
2023-12-14 12:29:06 -0500

    bugfix

--- cameraStream.py
+++ cameraStream.py
@@ -85,10 +85,8 @@ class StreamingServer(socketserver.ThreadingMixIn, server.HTTPServer):
 
 with picamera2.Picamera2() as camera:
     capture_config = camera.create_video_configuration(
-        main={"size": (640, 480),
-        "format"="mjpeg"},
-        lores={"size": (640, 480),
-        "format"="mjpeg"},
+        main={"size": (640, 480), "format": "mjpeg"},
+        lores={"size": (640, 480), "format": "mjpeg"},
         display="lores",
     )
     camera.configure(capture_config)
